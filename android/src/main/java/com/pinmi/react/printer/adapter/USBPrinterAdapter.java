54687ddcc2d0acf0b544fe341fe3be7bfe1dcff0
Shahid Helal <shahidhelal990@gmail.com>
2024-05-01 03:17:34 -0500

    fix:arabic

--- android/src/main/java/com/pinmi/react/printer/adapter/USBPrinterAdapter.java
+++ android/src/main/java/com/pinmi/react/printer/adapter/USBPrinterAdapter.java
@@ -251,7 +251,7 @@ public class USBPrinterAdapter implements PrinterAdapter {
         int bitmapHeight = height;
         Bitmap textBitmap = Bitmap.createBitmap(bitmapWidth, bitmapHeight, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(textBitmap);
-        canvas.drawColor(Color.argb(0, 255, 255, 255));
+        canvas.drawColor(Color.WHITE);
         Paint paint = new Paint();
         paint.setColor(Color.BLACK); // Set the text color
         paint.setTextSize(16f); // Set the text size
